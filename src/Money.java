6b371d8245c30034737c20da5dc74ce28b79c6ad
VictorHSMoura <victorhugosmoura@gmail.com>
2020-09-30 16:55:25 -0300

    Commit #4

--- src/Money.java
+++ src/Money.java
@@ -1,12 +1,18 @@
 abstract public class Money {
     protected int amount;
+    protected String currency;
 
     static Dollar dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Franc franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
     abstract Money times(int multiplier);
@@ -14,5 +20,9 @@ abstract public class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    public String currency() {
+        return currency;
     }
 }
